c7844c92e1faaaa1d80cdb9e326e007cbfe7a8cb
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-16 22:56:20 +0100

    user schema modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter, Montserrat } from 'next/font/google'
 import './globals.css'
 import { PortfolioContextProvider } from '@/context/PortfolioContext'
+import AuthProvider from '@/context/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 const montserrat = Montserrat({ subsets: ['latin'] })
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} bg-bgColor`}>
-        <PortfolioContextProvider>
-          {children}
-        </PortfolioContextProvider>
+        <AuthProvider>
+          <PortfolioContextProvider>
+            {children}
+          </PortfolioContextProvider>
+        </AuthProvider>
       </body>
     </html>
   )
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,11 @@
-
+"use client"
+import { signIn } from "next-auth/react"
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      <h2>Log in to leave a review for Kelechi</h2>
+      <button className="bg-primary rounded-md p-4" onClick={() => signIn()}>Sign in with Google</button>
+    </div>
   )
 }
 
