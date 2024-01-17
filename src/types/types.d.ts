a8e969dd362408a08c53ab4efe8058c44b5dfcb2
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-17 03:42:49 +0100

    session type updated

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,10 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { User } from "@prisma/client";
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
-    user: User; 
+    user: DefaultSession & User; 
   }
 }
 
