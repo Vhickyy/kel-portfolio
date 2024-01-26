fa2b5b18b340d10f287620217925720292b89955
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-01-26 13:37:44 +0100

    added video pages

---
+++ src/app/dashboard/video/component/UploadVideo.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { addProject } from "@/utils/actions"
+import { useFormState,useFormStatus } from "react-dom"
+const UploadBtn = () => {
+    const {pending} = useFormStatus()
+    return(
+        <button className='bg-primary px-8 py-2 rounded-md text-black' disabled={pending}>{pending ? "uploading..." : "Upload"}</button>
+    )
+}
+
+const UploadVideo = () => {
+const [uploadMsg, FormUpload] = useFormState(addProject,{message:""})
+  return (
+    <form action={FormUpload}>
+        {/* {uploadMsg ? <p>{uploadMsg.message}</p> : null} */}
+        <div>
+            <label htmlFor="upload" className='block'>Upload Video</label>
+            <input type="file" name='upload' id='upload'/>
+        </div>
+        <div>
+            <label htmlFor="title" className='block'>Title</label>
+            <input type="text" name='title' id='title' className='text-black p-2'/>
+        </div>
+        <div>
+            <label htmlFor="description" className='block'>Description</label>
+            <input type="text" name='description' id='description' className='text-black p-2'/>
+        </div>
+        <div>
+            <label htmlFor="link" className='block'>Link</label>
+            <input type="text" name='link' id='link' className='text-black p-2'/>
+        </div>
+       <UploadBtn/>
+    </form>
+  )
+}
+
+export default UploadVideo
---
+++ src/app/dashboard/video/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UploadProject from '../profile/component/UploadProject'
+import UploadVideo from './component/UploadVideo'
+
+const page = () => {
+  return (
+    <section className='text-textColor'>
+        <h1 >Welcome Kelechi</h1>
+        <UploadVideo/>
+    </section>
+  )
+}
+
+export default page
